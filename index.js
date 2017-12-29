78c20a1b5b0bde25a6324364b996c9d22e662344
Prathamesh0000 <33022940+Prathamesh0000@users.noreply.github.com>
2017-12-29 17:06:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var tickInt=50	;
-//<--choice-->
+
+var tickInt=5	;
+//<--choice(to)-->
 var database =[[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0],		//<--carNow?
-							[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0],
-							[0,0,0,0,0,0,0,0]]
+			[0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0],		//<--carNow?(from)
+			[0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0]]
 
 $(document).ready(function(){
 var tick = setInterval(function(){
@@ -17,6 +18,8 @@ var tick = setInterval(function(){
 	checkSensor("sensor_3");
 	checkCollision()
 	//moveCar()
+
+	move().car()
 },tickInt)
 
 });
@@ -33,6 +36,9 @@ var roadHeight="400";
 var now=parseInt($('#block_1').css("right").slice(0, -2));
 var collisionTemp=false;
 var moveObj=move();
+var posFrom=positionNow()
+var posTo=positionNow()
+var moved=false;
 
 function MaxInArray(temp){
 	var index = 0;
@@ -88,12 +94,35 @@ function move() {
 var moveBy=2;
 var moveToBlock=50/moveBy;
 
+	function car(){
+		if((sensorData.sensor_1||sensorData.sensor_2||sensorData.sensor_3)){
+			if(!moved){
+			heuMove()
+			moved=true;
+			}
+		}
+	}
 	function heuMove(){
 		var leastInd=MaxInArray(database[positionNow()])
-		position(leastInd[Math.floor(Math.random() * (leastInd.length+1))])
+		var now=positionNow()
+		var minDistance=1000;
+		var minDistanceInd;
+		leastInd.forEach(function(val){
+			var modDistance=Math.abs(val-now)
+			if(minDistance>modDistance){
+				minDistance=modDistance	
+				minDistanceInd=val;
+				
+			}
+		})
+
+		position(minDistanceInd)
 	}
 	function position(y){
 		var x = positionNow()
+		posFrom=x;
+		posTo=y;
+
 		if(x<y){
 			for(var i=x;i<y;i++){
 				blockDown()
@@ -152,7 +181,9 @@ var moveToBlock=50/moveBy;
 		blockUp:blockUp,
 		blockDown,blockDown,
 		heuMove:heuMove,
-		position:position
+		position:position,
+		car:car
+
 	}
 }
 
@@ -166,8 +197,19 @@ function movetick(){
 		$('#block_1').css("right",now);
 }
 	else{
+
 		now=0;
 		//moveUp()
+		
+		if(moved){
+			if(collisionTemp){
+				database[posFrom][posTo]-=1;
+			}else{
+				database[posFrom][posTo]+=1;
+			}
+		}
+		moved=false;
+		
 		if(collisionTemp){
 			collisionTemp=false;
 			collision++;
@@ -176,6 +218,8 @@ function movetick(){
 		blockPosUpLeft=Math.random()*300;
 		$('#block_1').css("margin-top",blockPosUpLeft);
 		$('#block_1').css("right",now);
+		 posFrom=positionNow()
+		 posTo=positionNow()
 
 	}
 clearSensor()
@@ -187,6 +231,7 @@ function clearSensor(){
 		$("#"+i).css("background-color","white");
 		sensorData[i]=false;
 		$("#"+i+"_table").html("false").css("color","red");
+
 
 	}
 
